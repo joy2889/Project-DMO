b6d08b9449cf45c970c39c0ce3d1fd3c8976ef02
joy2889 <45876565+joy2889@users.noreply.github.com>
2022-06-13 11:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask
 from flask import render_template, url_for, flash, redirect, request, Response
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from datetime import datetime
   
 app = Flask(__name__)
-
-cred = credentials.Certificate('servicekey.json')
-firebase_admin.initialize_app(cred, {'databaseURL': "https://project-dmo-default-rtdb.asia-southeast1.firebasedatabase.app"})
   
 
 @app.route('/')
@@ -27,15 +20,9 @@ def handle_data():
         instr = instr.replace(" ", "")
     else:
         instr = instr.replace(" ", ",")
-    
-    today = datetime.now()
-    today = str(today.strftime("%b-%d-%Y %H:%M:%S"))
-    ref = db.reference('/')
-    ref.child(today).set(instr)
-    
     return render_template(r'home.html', data = instr)
   
 
 if __name__ == '__main__':
     
-    app.run(debug=True)
+    app.run()
